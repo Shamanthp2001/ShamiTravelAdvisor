3a4adf08ded2c6dcbc3785977d63887eb2167982
Shamanth P <106751831+Shamanthp2001@users.noreply.github.com>
2024-12-26 19:02:21 +0530

    Update Hotel.java

--- TravelAdviser/src/main/java/com/shami/travel_advisor/hotel/Hotel.java
+++ TravelAdviser/src/main/java/com/shami/travel_advisor/hotel/Hotel.java
@@ -1,46 +1,26 @@
 package com.shami.travel_advisor.hotel;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalTime;
+@Setter
+@Getter
+@NoArgsConstructor //for JPA /JDBC to interact with replication
+@AllArgsConstructor // for update other attributes in hotel to access
+@Entity //table name default taken class name
+//@Table(name = "hotel_table") // if you want to change name of tble
 public class Hotel {
+
+    @Id // Primary Key
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String location;
     private LocalTime inTime;
     private LocalTime outTime;
     private Integer price;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public LocalTime getInTime() {
-        return inTime;
-    }
-
-    public LocalTime getOutTime() {
-        return outTime;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-
-    public Hotel(Integer id, String name, String location, LocalTime inTime, LocalTime outTime, Integer price) {
-        this.id = id;
-        this.name = name;
-        this.location = location;
-        this.inTime = inTime;
-        this.outTime = outTime;
-        this.price = price;
-    }
-
 }
