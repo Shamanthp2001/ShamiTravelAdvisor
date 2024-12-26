0a3f5b98bea7adbd32c92d162b09346249012ee5
Shamanth P <106751831+Shamanthp2001@users.noreply.github.com>
2024-12-26 19:04:15 +0530

    Update HotelService.java

--- TravelAdviser/src/main/java/com/shami/travel_advisor/hotel/HotelService.java
+++ TravelAdviser/src/main/java/com/shami/travel_advisor/hotel/HotelService.java
@@ -3,6 +3,10 @@ package com.shami.travel_advisor.hotel;
 import java.util.List;
 
 public interface HotelService {
-    void createHotel(Hotel hotel);
+    void createHotels(List<Hotel> hotel);
     List<Hotel> findAllHotels();
+    Hotel getHotelById(Integer id);
+    boolean updateHotelById(Integer id, Hotel hotel);
+
+    boolean deleteHotelById(Integer id);
 }
